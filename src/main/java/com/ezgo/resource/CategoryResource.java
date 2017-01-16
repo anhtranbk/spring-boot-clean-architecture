da23c05971953464e0bd1f5614a4e5937eb07545
tjeubaoit <anhtn.bk@gmail.com>
2017-01-16 20:38:12 +0700

    Update API definition

--- src/main/java/com/ezgo/resource/CategoryResource.java
+++ src/main/java/com/ezgo/resource/CategoryResource.java
@@ -3,11 +3,7 @@ package com.ezgo.resource;
 import com.ezgo.domain.Categories;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,8 +37,9 @@ public class CategoryResource {
     }
 
     @PUT
-    @Path("/categories")
-    public Response updateCategory(Categories category) {
+    @Path("/categories/{categoryId}")
+    public Response updateCategory(@PathParam("categoryId") String categoryId,
+                                   Categories category) {
         return Response.ok(category).build();
     }
 }
--- src/main/java/com/ezgo/resource/CommentResource.java
+++ src/main/java/com/ezgo/resource/CommentResource.java
@@ -3,13 +3,7 @@ package com.ezgo.resource;
 import com.ezgo.domain.Comments;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,8 +33,9 @@ public class CommentResource {
     }
 
     @PUT
-    @Path("/me/events/{eventId}/comments")
+    @Path("/me/events/{eventId}/comments/{commentId}")
     public Response updateComment(@PathParam("eventId") String eventId,
+                                  @PathParam("commentId") String commentId,
                                   Comments comment) {
         return Response.ok(comment).build();
     }
