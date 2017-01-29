1e03a6702cfe6edbc6874784e8c7b2953786596a
tjeubaoit <anhtn.bk@gmail.com>
2017-01-29 14:02:30 +0700

    BIG UPDATE: move domain + resource packages into app/

---
+++ src/main/java/com/ezgo/app/resource/UserResource.java
@@ -0,0 +1,33 @@
+package com.ezgo.app.resource;
+
+import com.ezgo.app.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ * TODO: Class description here.
+ *
+ * @author <a href="https://github.com/tjeubaoit">tjeubaoit</a>
+ */
+@Component
+@Path("/me")
+public class UserResource {
+
+    @Value("${root_photo_path}")
+    private String rootPhotoPath;
+
+    @GET
+    public Response me() {
+        return Response.ok(rootPhotoPath).build();
+    }
+
+    @PUT
+    public Response updateUser(User user) {
+        return Response.ok().build();
+    }
+}
