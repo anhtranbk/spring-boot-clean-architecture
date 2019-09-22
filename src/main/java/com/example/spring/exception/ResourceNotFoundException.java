1bab62d443ee25ebda27bc9cdcef50d969eb9027
tjeubaoit <anhtn.bk@gmail.com>
2019-09-22 11:31:18 +0700

    Update common exception ResourceNotFound

--- src/main/java/com/example/spring/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/spring/exception/ResourceNotFoundException.java
@@ -4,6 +4,21 @@ import org.springframework.http.HttpStatus;
 
 public class ResourceNotFoundException extends BaseException {
 
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
     @Override
     protected int getStatusCode() {
         return HttpStatus.NOT_FOUND.value();
